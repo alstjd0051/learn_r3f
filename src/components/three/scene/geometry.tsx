bc5f0e15a59e32e325bbd7ad6f99b90d2ca27cb1
alstjd0051 <wsc03002@naver.com>
2024-05-11 00:55:12 +0900

    feat: Add lazy loading for images and update npm dependencies

--- src/components/three/scene/geometry.tsx
+++ src/components/three/scene/geometry.tsx
@@ -1,7 +1,7 @@
-import { Box } from "@react-three/drei";
-import { Vector3 } from "@react-three/fiber";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { Vector3 } from "@react-three/fiber";
+import { useControls } from "leva";
 
 interface MyBoxProps {
   children: React.ReactNode;
@@ -17,11 +17,64 @@ function MyBox({ children, position }: MyBoxProps) {
 }
 
 const Geometry = () => {
+  const refMesh = useRef<THREE.Mesh>(null);
+  const refWireMesh = useRef<THREE.Mesh>(null);
+
+  const { xSize, xSegments, ySegments, ySize, zSegments, zSize } = useControls({
+    xSize: {
+      value: 1,
+      min: 0.1,
+      max: 5,
+      step: 0.01,
+    },
+    ySize: {
+      value: 1,
+      min: 0.1,
+      max: 5,
+      step: 0.01,
+    },
+    zSize: {
+      value: 1,
+      min: 0.1,
+      max: 5,
+      step: 0.01,
+    },
+    xSegments: {
+      value: 1,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+    ySegments: {
+      value: 1,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+    zSegments: {
+      value: 1,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+  });
+
+  useEffect(() => {
+    if (!refMesh.current || !refWireMesh.current) return;
+    refWireMesh.current.geometry = refMesh.current?.geometry;
+  }, [xSize, xSegments, ySegments, ySize, zSegments, zSize]);
+
   return (
     <>
       <ambientLight intensity={0.4} />
       <directionalLight position={[2, 1, 3]} intensity={0.5} />
-      <mesh>
+      <mesh ref={refMesh}>
+        <boxGeometry
+          args={[xSize, ySize, zSize, xSegments, ySegments, zSegments]}
+        />
+        <meshStandardMaterial color={"#1abc9c"} />
+      </mesh>
+      <mesh ref={refWireMesh}>
         <boxGeometry />
         <meshStandardMaterial
           emissive={"yellow"}
@@ -29,10 +82,6 @@ const Geometry = () => {
           color={"#1abc9c"}
         />
       </mesh>
-
-      <Box>
-        <meshStandardMaterial color={"#8e44ad"} />
-      </Box>
 
       <MyBox position={[-1.2, 0, 0]}>
         <meshStandardMaterial color={"#e74c3c"} />
