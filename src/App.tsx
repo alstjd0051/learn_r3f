e9235d731821f88aea03451148b9bc2d9113c7c7
alstjd0051 <wsc03002@naver.com>
2024-05-13 03:58:01 +0900

    feat: Add react-router-dom for routing in the application

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import ThreeWrapper from "./components/three/wrapper";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useChangeTools, useClient } from "./components/lib/tanstack";
+import { Route, Routes } from "react-router-dom";
+import ErrorBoundary from "./components/units/error";
 
 function App() {
   const queryClient = useClient();
@@ -12,7 +14,15 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ThreeWrapper />
+        <Routes>
+          <Route
+            path="/"
+            element={<ThreeWrapper />}
+            errorElement={<ErrorBoundary />}
+          />
+          <Route path="/:id" element={<ErrorBoundary />} />
+        </Routes>
+
         <Suspense fallback={<h1>Loading...</h1>}>
           <ReactQueryDevtools
             initialIsOpen={false}
