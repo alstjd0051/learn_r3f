2554f2685f55dd35fb3e13b7c24d518dc8f79d0a
alstjd0051 <wsc03002@naver.com>
2024-05-26 21:29:02 +0900

    refactor: Update ThreeWrapper component to handle different camera settings based on selected component

--- src/components/three/wrapper.tsx
+++ src/components/three/wrapper.tsx
@@ -1,14 +1,21 @@
 import { Canvas } from "@react-three/fiber";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { OrbitControls } from "@react-three/drei";
-import { Vector3 } from "three";
+
 import { useRtfCollection } from "../hooks/rtfcollection";
+import { Vector3dArray } from "leva/dist/declarations/src/types";
 
 const ThreeWrapper = () => {
-  const [cameraSettings, setCameraSettings] = useState({
+  const [cameraSettings, setCameraSettings] = useState<{
+    fov: number;
+    position: Vector3dArray | undefined;
+    near?: number;
+    far?: number;
+  }>({
     fov: 75,
-    position: new Vector3(7, 7, 0),
+    position: [7, 7, 0],
   });
+  const CanvasRef = useRef<HTMLCanvasElement>(null);
 
   const { data, SelectedComponent, handleClick } = useRtfCollection();
 
@@ -21,19 +28,26 @@ const ThreeWrapper = () => {
       if (SelectedComponent.name.includes("Light")) {
         setCameraSettings({
           fov: 75,
-          position: new Vector3(7, 7, 0),
+          position: [7, 7, 0],
+        });
+      } else if (SelectedComponent.name === "Camera") {
+        setCameraSettings({
+          fov: 130,
+          near: 0.1,
+          far: 20,
+          position: [7, 7, 0],
         });
       } else {
         setCameraSettings({
           fov: 60,
-          position: new Vector3(5, 5, 5),
+          position: [5, 5, 5],
         });
       }
     }
   }, [SelectedComponent]);
 
   return (
-    <div className="w-full">
+    <div className="w-full h-dvh overflow-hidden">
       <nav className="grid grid-cols-2 gap-y-3 md:gap-y-5 md:grid-cols-5   gap-x-5 max-w-2xl md:max-w-3xl xl:max-w-4xl mx-auto ">
         {data?.map(({ name }, idx) => (
           <button
@@ -48,8 +62,8 @@ const ThreeWrapper = () => {
         ))}
       </nav>
 
-      <Canvas style={{ height: `100dvh` }} camera={{ ...cameraSettings }}>
-        <OrbitControls />
+      <Canvas ref={CanvasRef} camera={cameraSettings}>
+        {SelectedComponent?.name !== "Camera" && <OrbitControls />}
         <ChangeThreeItems />
       </Canvas>
     </div>
